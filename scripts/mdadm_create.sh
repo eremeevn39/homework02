42c9a6bba3f84780601fdef241bc64744c02375e
eremeevn39 <eremeevn@gmail.com>
2020-11-22 22:44:23 +0200

    edited script

--- scripts/mdadm_create.sh
+++ scripts/mdadm_create.sh
@@ -14,5 +14,4 @@ parted /dev/md0 mkpart primary ext4 80% 100%
 for i in $(seq 1 5); do sudo mkfs.ext4 /dev/md0p$i; done
 mkdir -p /raid/part{1,2,3,4,5}
 for i in $(seq 1 5); do mount /dev/md0p$i /raid/part$i; done
-#echo -e "/dev/md0p1     /raid/part1     ext4    defaults        0       0\n/dev/md0p2   /raid/part2     ext4    defaults        0       0\n/dev/md0p3   /raid/part3     ext4    defaults        0       0\n/dev/md0p4   /raid/part4     ext4    defaults        0       0\n/dev/md0p5   /raid/part5     ext4    defaults        0       0" >> /etc/fstab
 for i in $(seq 1 5); do echo -e "/dev/md0p$i	/raid/part$i	ext4	defaults	0	0" >> /etc/fstab;done
